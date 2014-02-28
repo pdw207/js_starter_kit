429d1a8eb5b7f35ee85a206a0ed07642e3272d1a
pdw207 <pdw207@gmail.com>
2014-02-28 14:05:58 -0500

    This is done!

--- js/app.js
+++ js/app.js
@@ -1,1 +1,24 @@
-alert('Hey find me to know where to put the javascript');
+alert('Welcome to the coolest game ever');
+
+var randomNumber = Math.round(Math.random()*100);
+
+
+// alert("The random number is " + randomNumber);
+
+var name = prompt('Hey, give me a name.');
+var number = prompt('Please enter a number!');
+
+// while (number !== randomNumber)
+//   {
+//   if(number == randomNumber){
+//     alert( name + "! Dude, you have won!");
+//     break;
+//   } else {
+
+//     return number = prompt("Try Again!!! Enter another number.");
+//   }
+//   }
+while (Number(number) !== randomNumber){
+  number = prompt("Try Again!!! Enter anther number");
+}
+alert('You won!');
